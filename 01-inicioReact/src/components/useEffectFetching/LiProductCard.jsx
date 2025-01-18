e45c2e2ef39a2c5dd11a4d6f454afe84089bd57f
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-01-18 15:40:17 +0100

    Create function handleClick

--- 01-inicioReact/src/components/useEffectFetching/LiProductCard.jsx
+++ 01-inicioReact/src/components/useEffectFetching/LiProductCard.jsx
@@ -1,17 +1,24 @@
+import { Button } from "./Button";
 
 const LiProductCard = (props) => {
-  const { product, index } = props;
-    
+  const { product, index, removeCart } = props;
+
+const handleClick = () => {
+  removeCart(product.id)
+}
   return (
-    <li key={index} className="bg-gray-300 shadow-lg rounded-lg p-6 flex flex-col">
-      <span className="text-2xl text-blue-500 font-medium font-semibold">
-      {product.title} 
-      </span>
-      <span className="text-1xl text-green-600">
-      Precio: {product.price}
-      </span>
-      <button className="bg-slate-600 hover:bg-slate-900 px-4 py-2 rounded-lg text-white">Quitar del carrito</button>
-    </li>
+    <ul>
+      <li key={index} className="bg-gray-300 shadow-lg rounded-lg p-6 flex flex-col">
+        <span className="text-2xl text-blue-500 font-medium font-semibold">
+          {product.title}
+        </span>
+        <span className="text-1xl text-green-600">
+          Precio: {product.price}
+        </span>
+        <Button onClick={() => handleClick(product.id)} className="bg-slate-600 hover:bg-slate-900 px-4 py-2 rounded-lg text-white">Quitar del carrito</Button>
+      </li>
+    </ul>
+
   )
 }
 
