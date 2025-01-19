613cb9c152bce471743629e2309a92064cc397d7
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-01-19 11:13:11 +0100

    Delete unnecessary lines

--- 01-inicioReact/src/App.jsx
+++ 01-inicioReact/src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from "react"
-import Hijo from "./components/parametros/Hijo"
-import Padre from "./components/parametros/Padre"
-import { Saludo } from "./components/useEffect/Saludo"
 import { ProductList } from "./components/useEffectFetching/ProductList"
 
-const initialStateInfo = {nombre: "Ismael", edad: 24, isAdmin: "false"}
 const App = () => {
-  const [info, setInfo] = useState(initialStateInfo)
-  const handleClickEdad = () => {
-    setInfo((prevInfo) => ({ ...prevInfo, edad: prevInfo.edad + 1 }));
-  };
+
   return (
     <>
-    {/* <p>El nombre es: {info.nombre}</p>
-    <Padre info={ info } setInfo={ setInfo } handleClickEdad={ handleClickEdad }>
-      <Hijo info={ info } handleClickEdad={ handleClickEdad }/>
-    </Padre> */}
-    {/* <Saludo /> */}
     <ProductList/>
     </>
   )
